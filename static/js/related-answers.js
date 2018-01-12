0f6f1cd26885bf635f531e82ee671b076845a535
Meganster <ppro100takk@mail.ru>
2018-01-12 12:48:45 +0300

    Update related-answers.js

--- static/js/related-answers.js
+++ static/js/related-answers.js
@@ -12,14 +12,26 @@
             }
         ).done(function (data) {
             console.log(data);
+            /*//var inf = jQuery.parseJSON(data);
+            var inf = data;
+            var p01 = document.getElementById('thread-answer');
+            alert('#' + p01.firstElementChild.id);
+            //var $newanswer = p01.firstElementChild.clone();
+            $('#thread-answer').innerHTML += p01.firstElementChild.outerHTML;
+            alert(inf.id);
+            p01.lastElementChild.id = 'answer_id-'+inf.id;
+            //p01.lastElementChild.find('#answer-create_date').text(inf.createdate);
+            //p01.lastElementChild.find('#answer-text').text($('#textQuestion').val());
+            //$('#thread-answer').append($newanswer);
+            $('#textQuestion').val('');*/
             var inf = data;
 
-            var $oldanswers = $('#answer_id-129').clone();
+            var $oldanswers = $('#answer_id-5').clone();
             $oldanswers.attr('id', 'answer_id-' + inf.id);
             $oldanswers.find('#answer-create_date').text(inf.createdate);
             $oldanswers.find('#answer-text').text($('#textQuestion').val());
             $('#thread-answer').append($oldanswers);
-            $('#textQuestion').val(' ');
+            $('#textQuestion').val('');
         });
     })
 })();
